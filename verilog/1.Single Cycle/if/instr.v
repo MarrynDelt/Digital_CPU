d5548069e844d38e0464d401024c72ea169f1eec
MarrynDelt <1072024185@qq.com>
2021-07-09 22:55:02 +0800

    Instruction Memory

--- verilog/1.Single Cycle/if/instr.v
+++ verilog/1.Single Cycle/if/instr.v
@@ -13,14 +13,13 @@ module instr(
 	output reg 	[31:0]	instr_out
 );
 
+	reg [7:0] instr_mem[0:255];
+
+	initial begin
+		/*insrt_mem is empty*/
+	end
+
 	always@(addr) begin
-		case(addr)
-
-			/*here is the instr*/
-
-			default:	begin
-							instr_out <= 32'h0;
-						end
-		endcase
+		instr_out <= {instr_mem[addr+3],instr_mem[addr+2],instr_mem[addr+1],instr_mem[addr]}
 	end
 endmodule
