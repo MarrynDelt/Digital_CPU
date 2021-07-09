afc3f5bc60c51ffe287c4f11ca6daf8300c2a353
MarrynDelt <1072024185@qq.com>
2021-07-09 19:10:45 +0800

    Instruction Fetch

--- verilog/1.Single Cycle/if/pc.v
+++ verilog/1.Single Cycle/if/pc.v
@@ -2,6 +2,7 @@
 //Single Cycle CPU
 //Created by Chesed
 //2021.07.08
+//Edited in 2021.07.09
 
 
 
@@ -10,6 +11,12 @@ module pc(
 	
 	input 				clk			,
 	input 				rst_n		,
+
+	input				jmp_en		,
+	input				jmpr_en		,
+	input				jmpj_en		,
+
+	input	[31:0]		jmp_to		,
 
 
 	output reg [31:0]	addr
@@ -25,12 +32,25 @@ module pc(
 
 	always @(posedge clk or negedge rst_n)begin
 		if(!rst_n) begin
-			addr => 32'h0;
 			addr_next => 32'h0;
 		end
 		else begin
-			addr_next => addr_next + 32'h4;
-			addr => addr_next;
+			if(jmp_en) begin
+				addr_next => addr + jmp_to;
+			end
+			else begin
+				if(jmpr_en) begin
+					addr_next => jmp_to;
+				end
+				else begin
+					if(jmpb_en) begin
+						addr_next => addr + jmp_to;
+					end
+					else begin
+						addr_next => addr + 32'd4;
+					end
+				end
+			end
 		end
 	end
 
--- verilog/1.Single Cycle/if/readme.txt
+++ verilog/1.Single Cycle/if/readme.txt
@@ -1,8 +1,14 @@
 if module for cpu
 
 
-ver1.0:
+ver 1.0:
 
 instr.v are empty
 pc don't support jump instruction
 2021.07.08
+
+
+ver 1.1:
+jump instruction is supported
+2021.07.09
+
